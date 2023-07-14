3ba4219a443d452edaad8229eee8cc3d799b9aed
Manuel <manuelsimma@gmail.com>
2023-07-14 15:48:07 +0200

    feat(image): added a part of Image

--- src/Fields/Image.php
+++ src/Fields/Image.php
@@ -3,6 +3,9 @@
 namespace DigitOne\Acf\Fields;
 
 use DigitOne\Acf\BaseField;
+use DigitOne\Acf\OptionTraits\AriaLabel;
+use DigitOne\Acf\OptionTraits\Required;
+use DigitOne\Acf\OptionTraits\ReturnFormat;
 
 class Image extends BaseField
 {
@@ -10,4 +13,9 @@ class Image extends BaseField
     protected $name = 'image';
     protected $label = 'Bild';
     protected $type = 'image';
+
+    use AriaLabel;
+    use Required;
+    use ReturnFormat;
+    
 }
