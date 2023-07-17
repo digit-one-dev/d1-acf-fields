7d973d2980f1f4900c835adc8c73b6fb680f0d57
Manuel <manuelsimma@gmail.com>
2023-07-17 15:21:59 +0200

    feat(acf-fields):added options to CloneField

--- src/Fields/CloneField.php
+++ src/Fields/CloneField.php
@@ -3,6 +3,11 @@
 namespace DigitOne\Acf\Fields;
 
 use DigitOne\Acf\BaseField;
+use DigitOne\Acf\OptionTraits\CloneOption;
+use DigitOne\Acf\OptionTraits\Display;
+use DigitOne\Acf\OptionTraits\Layout;
+use DigitOne\Acf\OptionTraits\PrefixLabel;
+use DigitOne\Acf\OptionTraits\PrefixName;
 
 class CloneField extends BaseField
 {
@@ -13,4 +18,10 @@ class CloneField extends BaseField
     protected $args = [
         'clone' => []
     ];
+
+    use Layout;
+    use CloneOption;
+    use Display;
+    use PrefixName;
+    use PrefixLabel;
 }
