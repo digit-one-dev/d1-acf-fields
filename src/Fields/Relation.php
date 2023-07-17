ef48770a9ee328f76f5267254f7b346d397a8367
Manuel <manuelsimma@gmail.com>
2023-07-17 13:28:46 +0200

    feat(acf-fields):added options to PostLink & Relations

--- src/Fields/Relation.php
+++ src/Fields/Relation.php
@@ -3,6 +3,14 @@
 namespace DigitOne\Acf\Fields;
 
 use DigitOne\Acf\BaseField;
+use DigitOne\Acf\OptionTraits\Filters;
+use DigitOne\Acf\OptionTraits\Max;
+use DigitOne\Acf\OptionTraits\Min;
+use DigitOne\Acf\OptionTraits\PostStatus;
+use DigitOne\Acf\OptionTraits\PostType;
+use DigitOne\Acf\OptionTraits\ReturnFormat;
+use DigitOne\Acf\OptionTraits\Taxonomy;
+use DigitOne\Acf\OptionTraits\Elements;
 
 class Relation extends BaseField
 {
@@ -14,4 +22,13 @@ class Relation extends BaseField
         'filters'   => [ 'search' ],
         'return_format' => 'id',
     ];
+
+    use PostType;
+    use PostStatus;
+    use Taxonomy;
+    use Filters;
+    use ReturnFormat;
+    use Min;
+    use Max;
+    use Elements;
 }
