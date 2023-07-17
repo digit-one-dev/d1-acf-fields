7aeb99f1391d692c45be496981929ebaab5df329
Manuel <manuelsimma@gmail.com>
2023-07-17 13:57:11 +0200

    feat(acf-fields):added options to Date&Timepicker

--- src/Fields/DateTimePicker.php
+++ src/Fields/DateTimePicker.php
@@ -3,6 +3,9 @@
 namespace DigitOne\Acf\Fields;
 
 use DigitOne\Acf\BaseField;
+use DigitOne\Acf\OptionTraits\DisplayFormat;
+use DigitOne\Acf\OptionTraits\FirstDay;
+use DigitOne\Acf\OptionTraits\ReturnFormat;
 
 class DateTimePicker extends BaseField
 {
@@ -15,4 +18,8 @@ class DateTimePicker extends BaseField
         'return_format'  => 'd.m.Y H:i',
         'first_day'      => 1,
     ];
+
+    use DisplayFormat;
+    use ReturnFormat;
+    use FirstDay;
 }
