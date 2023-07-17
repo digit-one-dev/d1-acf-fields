c3dc18202d91856c20991c31dac6390372e8905b
Manuel <manuelsimma@gmail.com>
2023-07-17 13:47:35 +0200

    feat(acf-fields):added options to GoogleMap

--- src/Fields/GoogleMap.php
+++ src/Fields/GoogleMap.php
@@ -3,6 +3,10 @@
 namespace DigitOne\Acf\Fields;
 
 use DigitOne\Acf\BaseField;
+use DigitOne\Acf\OptionTraits\CenterLat;
+use DigitOne\Acf\OptionTraits\CenterLng;
+use DigitOne\Acf\OptionTraits\Height;
+use DigitOne\Acf\OptionTraits\Zoom;
 
 class GoogleMap extends BaseField
 {
@@ -10,4 +14,9 @@ class GoogleMap extends BaseField
     protected $name = 'google_map';
     protected $type = 'google_map';
     protected $label = 'Goolge Map';
+
+    use CenterLat;
+    use CenterLng;
+    use Zoom;
+    use Height;
 }
